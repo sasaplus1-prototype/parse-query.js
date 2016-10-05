664672b472b90ff594449a8621a5be097fd1342a
sasaplus1 <sasaplus1@gmail.com>
2016-10-05 15:20:17 +0900

    added index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var toString = Object.prototype.toString;
+
+/**
+ * parse query string
+ *
+ * @param {String} text
+ * @throws {TypeError}
+ * @return {Object}
+ */
+function parseQuery(text) {
+  var result, params, i, len, key, value;
+
+  if (typeof text !== 'string' && toString.call(text) !== '[object String]') {
+    throw new TypeError('text must be a String');
+  }
+
+  result = {};
+  params = text.split('&');
+
+  for (i = 0, len = params.length; i < len; ++i) {
+    /=/.exec(params[i]);
+
+    key = RegExp.leftContext;
+    value = RegExp.rightContext;
+
+    result[key] || (result[key] = []);
+    result[key].push(
+      decodeURIComponent(value) || null
+    );
+  }
+
+  return result;
+}
+
+module.exports = parseQuery;
